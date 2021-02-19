08ff641416ab5bcfcc057a8175c351ab6c623e69
Remy Ogasawara <remyogasawara@remys-mbp.lan>
2021-02-19 00:13:53 -0800

    add file

---
+++ Lab4/Lab4.asm
@@ -0,0 +1,37 @@
+##########################################################################
+# Created by: Ogasawara, Remy
+#             rsogasaw
+#             18 February 2021
+#
+# Assignment: Lab 4: Syntax Checker 
+#             CSE 12, Computer Systems and Assembly Language
+#             UC Santa Cruz, Winter 2021
+#
+# Description: This program opens a .txt file and determines whether it 
+#              has balanced braces, brackets, and parentheses using MIPS 
+#              and stack. It will also report either the location of the 
+#              mismatch or the number of matched items on success. 
+#
+# Notes: This program is intended to be run from the MARS IDE and imports 
+#        .txt files using arguments
+#
+##########################################################################
+# Psuedocode:  
+#
+#
+#
+#
+#
+#
+#
+#
+##########################################################################
+.text 
+
+.data 
+# registers: 
+# $v0: holds the syscall number 
+# $a0: holds the printed value
+# $t0: holds the user input
+# $t1: holds the counter for patternLoop
+# $t2: holds the counter for starsLoops
