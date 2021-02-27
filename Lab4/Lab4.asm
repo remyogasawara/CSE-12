15a747370948a076c16da1a5b75493e1a6c73d7d
Remy Ogasawara <remyogasawara@remys-mbp.lan>
2021-02-27 02:30:37 -0800

    valid filenames

--- Lab4/Lab4.asm
+++ Lab4/Lab4.asm
@@ -61,7 +61,7 @@ fileMessage: .asciiz "You entered the file:\n"
 errorRemaining: .asciiz "ERROR - Brace(s) still on stack: " 
 errorMismatch: .asciiz "ERROR - There is a brace mismatch: "
 index: .asciiz " at index "
-invalid: .asciiz "ERROR: Invalid program argument." 
+invalidMessage: .asciiz "ERROR: Invalid program argument." 
 sucessMessage: .asciiz "SUCCESS: There are "
 pairs: .asciiz " pairs of braces."
 remainingBraces: .asciiz "ERROR - Brace(s) still on stack: " 
@@ -72,28 +72,79 @@ remainingBraces: .asciiz "ERROR - Brace(s) still on stack: "
 # $a0: holds the argument 
 # $a1: address of output buffer
 # $a2: max number of characters in file 
-# $s0: holds the contents of the txt file 
-# $s1: holds original stack location 
-# $t1: holds the temporary byte
-# $t2: holds the index number
-# $t3: holds the current $sp value
+# $s0: holds the file descriptor 
+# $s1: holds original stack location
+# $s2: holds contents of the txt file  
 
 main: 
      NOP 
      li     $v0 4 
      la     $a0 fileMessage 
-     syscall
+     syscall                 # prints "You entered the file:"
      
      lw     $s0 ($a1)
      move   $a0 $s0
      li     $v0 4
-     syscall
+     syscall                 # prints file name
      
-     # new line 
-     li $a0 10
-     li $v0 11 
-     syscall
+     li     $a0 10
+     li     $v0 11 
+     syscall                 # prints a  new line 
+     li     $a0 10
+     li     $v0 11 
+     syscall                 # prints a new line 
      
+     
+     li      $v0 1
+     lb      $a0 0   ($s0) 
+     move    $t1 $a0
+     
+     sge     $t2 $t1 97             # if first character is greater or equal to 97 
+     sle     $t3 $t1 122            # if first character is less or equal to 122
+     and     $t4 $t2 $t3            # return 1 if 97 <= $t1 <= 122
+     sge     $t2 $t1 65             # if first character is greater or equal to 65
+     sle     $t3 $t1 90             # if first character is less or equal to 90
+     and     $t5 $t2 $t1            # return 1 if 65 <= $t1 <= 90
+     or      $t6 $t4 $t5            # return 1 if 97 <= $t1 <= 122 or 65 <= $t1 <= 90
+     beq     $t6 0 invalid          # if not 97 <= $t1 <= 122 or 65 <= $t1 <= 90, invalid filename 
+     li      $t0 0                  # keeps track of how many characters there are 
+     checkFile: 
+         addi $t0 $t0 1             # increase counter  
+         lb $a0 0($s0)              # loads the character of the file name 
+         move $t1 $a0               # moves the character into $t1 
+         beq $t1 0 checkEnd         # end loop when null 
+         bgt $t0 20 invalid         # if the file is larger than 20 characters, it is invalid
+         
+         sge     $t2 $t1 97         # if first character is greater or equal to 97 
+         sle     $t3 $t1 122        # if first character is less or equal to 122
+         and     $t4 $t2 $t3        # characters a - z can be part of the file 
+         
+         sge     $t2 $t1 65         # if first character is greater or equal to 65
+         sle     $t3 $t1 90         # if first character is less or equal to 90
+         and     $t5 $t2 $t3        # characters A - Z can be part of the file 
+         
+         sge     $t2 $t1 48         # if first character is greater or equal to 65
+         sle     $t3 $t1 57         # if first character is less or equal to 90
+         and     $t6 $t2 $t3        # characters 0 - 9 can be part of the file 
+         
+         seq     $t7 $t1 46         # periods can be part of the file         
+         seq     $t8 $t1 95         # underscores can be part of the file
+         
+         or      $t2 $t4 $t5        # return 1 if 97 <= $t1 <= 122 or 65 <= $t1 <= 90
+         or      $t3 $t2 $t6
+         or      $t2 $t3 $t7
+         or      $t3 $t2 $t8 
+         #li $v0 1 
+         #move $a0 $t1
+         #syscall
+         #li $v0 11 
+         #la $a0 10
+         #syscall
+         beq $t3 0 invalid
+         addi $s0 $s0 1             # move to the next character 
+         j checkFile
+     checkEnd:
+      
      # get argument 
      lw      $s0 ($a1)       # move to file descriptor into $s0
      move    $a0 $s0         # move the argument into $a0 
@@ -108,31 +159,23 @@ main:
      li      $v0 14          # syscall for read file 
      move    $a0 $s0         # reads the file descriptor 
      la      $a1 buffer      # address of buffer from which to write 
-     la      $a2 20          # max char length 
+     li      $a2 128          # max char length 
      syscall                 # read file 
      
+     add    $s1 $0 $sp       # store the original stack value  
+     
+     li     $t4 0            # pair counter 
+     li     $t2 0            # stores the index
+     
      la      $a0 buffer      # address of buffer 
-     li      $v0 4
-     move    $s0 $a0         # move the contents of the txt into $s0
-     
-     # new line 
-     li     $a0 10
-     li     $v0 11 
-     syscall
-     
-     # store the empty stack
-     add $s1 $0 $sp           # store the original stack value 
-     
-     
-     li $s2 0 # pair counter 
-     li $t2 0 # stores the index
+     move    $s2 $a0         # move the contents of the txt into $s0  
      textLoop:
           li      $v0 1                    # sycall for ascii integer 
-          lb      $a0 ($s0)                # loads the byte into the $a0 register 
+          lb      $a0 ($s2)                # loads the byte into the $a0 register 
           move    $t1 $a0                  # moves to byte to the $t1 register 
           beq     $t1 0     textEnd       # ends the loops if the byte is null 
     
-          addi    $s0 $s0 1                # move to the next byte
+          addi    $s2 $s2 1                # move to the next byte
           addi    $t2 $t2 1                # increase the index 
           
           beq     $t1 40    PUSH           # if '(' push
@@ -159,7 +202,7 @@ success: #prints success message and exits the emptyStack label
      syscall          # prints "There are "
           
      li $v0 1 
-     move $a0 $s2
+     move $a0 $t4
      syscall          # prints number of pairs 
           
      li $v0 4         
@@ -181,9 +224,12 @@ remaining:
      
      stackLoop:
           beq $sp $s1 stackEnd  # exits when the stack is empty
+          
           lw $a0 ($sp) 
           li $v0 11
-          syscall
+          syscall               # print brace 
+          addi $sp $sp 4 
+          j stackLoop 
      stackEnd: 
      
      li      $a0 10
@@ -193,7 +239,17 @@ remaining:
      # end of remaining  
      li     $v0 10
      syscall
-       
+ 
+ invalid: 
+     li $v0 4 
+     la $a0 invalidMessage
+     syscall
+     li      $a0 10
+     li      $v0 11 
+     syscall               # new line  
+     # end of invalid  
+     li     $v0 10
+     syscall
           
 #################### STACK ####################
 
@@ -207,8 +263,7 @@ PUSH:
 POP:
     lw   $t1 ($sp)       # gets the byte from the stack 
     addi $sp $sp   4     # moves the stack pointer back up 
-    addi $s2 $s2   1     # increases the pair count    
-    
+    addi $t4 $t4   1     # increases the pair count    
     j    textLoop    
      
 checkStack: # checks the length of the stack
@@ -240,7 +295,7 @@ checkStack: # checks the length of the stack
           li     $v0 4 
           la     $a0 errorMismatch 
           syscall                   # print error message  
-           
+
           li     $v0 11
           move   $a0 $t1
           syscall                   # print brace
